ba6755280a8995e65083c0262837e4f2baba76bd
lilmayu <uwulilmayu@gmail.com>
2025-04-22 20:59:17 +0200

    feat: Initial fabric project setup

---
+++ src/client/java/enterprises/iwakura/simplydashkeybind/client/SimplyDashKeybindModClient.java
@@ -0,0 +1,10 @@
+package enterprises.iwakura.simplydashkeybind.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimplyDashKeybindModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/enterprises/iwakura/simplydashkeybind/SimplyDashKeybindMod.java
@@ -0,0 +1,10 @@
+package enterprises.iwakura.simplydashkeybind;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SimplyDashKeybindMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
